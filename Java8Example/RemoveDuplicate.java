085b79dfe1ffabedf3b6ce6241601f502a25cee8
saurabh706 <47627990+saurabh706@users.noreply.github.com>
2026-07-01 17:18:05 +0530

    Implement method to remove duplicates from array

--- Java8Example/RemoveDuplicate.java
+++ Java8Example/RemoveDuplicate.java
@@ -1,1 +1,12 @@
+// Remove Duplicate from Arrays
+import java.util.Arrays;
 
+public class RemoveDuplicate {
+    
+    public static void main(String[] args) {
+        Integer[] nums = {1, 2, 3, 4, 5, 1, 2, 3};
+
+        Integer[] list = Arrays.stream(nums).distinct().toArray(Integer[]::new);
+        System.out.println(Arrays.toString(list));
+    }
+}
